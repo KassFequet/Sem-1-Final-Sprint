77736dc24e43828bc9c252279c764cd26fe78d7e
KassFequet <kassaundra.fequet@keyin.com>
2025-04-10 14:41:36 -0230

    nav bar complete
    styled nav links and added a scrolling progress bar

--- Javascript/index.js
+++ Javascript/index.js
@@ -26,6 +26,9 @@ const com2Format = new Intl.NumberFormat("en-CA", {
   maximumFractionDigits: "2",
 });
 
+// Start function definitions here.
+
+// Function to create a progress bar for how far scrolled on page
 window.addEventListener("scroll", function () {
   const progressBar = document.getElementById("progress-bar");
   const scrollTop = window.scrollY;
@@ -35,27 +38,3 @@ window.addEventListener("scroll", function () {
   const scrollPercentage = (scrollTop / docHeight) * 100;
   progressBar.style.width = scrollPercentage + "%";
 });
-
-// Start function definitions here.
-
-function ShowGreeting() {
-  // Create the greeting based on the hour of the day.
-  // Use the getHour() method on the current date - in 24 hour clock.
-
-  let CurDate = new Date();
-  let CurHour = CurDate.getHours(); // This returns a value from 0-24
-
-  let Greeting = "";
-  if (CurHour >= 6 && CurHour <= 12) {
-    Greeting = "Good Morning";
-  } else if (CurHour >= 12 && CurHour <= 18) {
-    Greeting = "Good Afternoon";
-  } else if (CurHour >= 18 && CurHour <= 24) {
-    Greeting = "Good Evening";
-  } else {
-    Greeting = "Good Night";
-  }
-
-  Greeting += " - " + CurDate.toDateString();
-  document.writeln(Greeting);
-}
